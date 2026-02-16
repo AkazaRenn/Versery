2eaa0b87bdf1395339546e5d787dd4ed37012fb5
Renn <8340896+AkazaRenn@users.noreply.github.com>
2026-02-16 14:23:01 -0500

    Centralized version management

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+using Microsoft.UI.Xaml.Controls;
+using WinUIEx;
+
+namespace View;
+
+public sealed partial class MainWindow: WindowEx {
+    public MainWindow() {
+        InitializeComponent();
+    }
+}
