b42b40e815f77a9a1b1d7a1d2e769ea388c810b5
Renn <8340896+AkazaRenn@users.noreply.github.com>
2026-02-16 20:50:21 -0500

    Proper top nav

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
 using WinUIEx;
 
 namespace View;
@@ -6,5 +6,14 @@ namespace View;
 public sealed partial class MainWindow: WindowEx {
     public MainWindow() {
         InitializeComponent();
+
+        ExtendsContentIntoTitleBar = true;
+        AppWindow.TitleBar.PreferredHeightOption = Microsoft.UI.Windowing.TitleBarHeightOption.Tall;
+        SetTitleBar(titleBar);
+        titleBar.Margin = new Thickness(mainNavigation.CompactPaneLength, 0, 0, 0);
+        titleBar.Height = mainNavigation.CompactPaneLength;
+
+        MinWidth = 400;
+        MinHeight = 400;
     }
 }
