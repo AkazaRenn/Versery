dba6894395a19133c58451764cd2f9e4af91a1fe
Renn <8340896+AkazaRenn@users.noreply.github.com>
2026-02-17 20:08:48 -0500

    Add navigation

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using System.Security.AccessControl;
+using View.Pages;
+using Windows.UI;
+using Windows.UI.WindowManagement;
 using WinUIEx;
 
 namespace View;
@@ -9,11 +15,43 @@ public sealed partial class MainWindow: WindowEx {
 
         ExtendsContentIntoTitleBar = true;
         AppWindow.TitleBar.PreferredHeightOption = Microsoft.UI.Windowing.TitleBarHeightOption.Tall;
-        SetTitleBar(titleBar);
-        titleBar.Margin = new Thickness(mainNavigation.CompactPaneLength, 0, 0, 0);
-        titleBar.Height = mainNavigation.CompactPaneLength;
+        SetTitleBar(TitleBar);
+        TitleBar.Margin = new Thickness(MainNavigation.CompactPaneLength, 0, 0, 0);
+        TitleBar.Height = MainNavigation.CompactPaneLength;
 
         MinWidth = 400;
         MinHeight = 400;
+
+        navigationViewItem_SamplePage1.Tag = typeof(View.Pages.SamplePage1);
+        navigationViewItem_Login.Tag = typeof(View.Pages.Login);
+    }
+
+    private void MainNavigation_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args) {
+        if (args.SelectedItem is NavigationViewItem item) {
+            if (item.Tag is Type pageType) {
+                ContentFrame.Navigate(pageType);
+            }
+        }
+    }
+
+    private void MainNavigation_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args) {
+        if (ContentFrame.CanGoBack) {
+            ContentFrame.GoBack();
+        }
+    }
+
+    private void ContentFrame_Navigated(object sender, NavigationEventArgs e) {
+        MainNavigation.IsBackEnabled = ContentFrame.CanGoBack;
+
+        foreach (var item in MainNavigation.MenuItems.OfType<NavigationViewItem>()) {
+            if (item.Tag is Type pageType) {
+                if (pageType == e.SourcePageType) {
+                    MainNavigation.SelectedItem = item;
+                    return;
+                }
+            }
+        }
+
+        MainNavigation.SelectedItem = null;
     }
 }
