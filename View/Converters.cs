9555d2ceeb2a25897537badf089364a586d1aa10
Renn <8340896+AkazaRenn@users.noreply.github.com>
2026-03-02 21:17:35 -0500

    Simplify data convertion on the login page

---
+++ View/Converters.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace View; 
+internal static class Converters {
+    public static bool NullableObjectToBoolReversed(object? value) {
+        return value != null;
+    }
+
+    public static Visibility NullableObjectToVisibility(object? value) {
+        return value != null ? Visibility.Visible : Visibility.Collapsed;
+    }
+}
