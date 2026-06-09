cec4be44ceddff6a1a944a668ad91c2d5a464d99
Renn <8340896+AkazaRenn@users.noreply.github.com>
2026-06-08 21:58:25 -0400

    Use LiteDB

--- Model/DataPersistence/Database.cs
+++ Model/DataPersistence/Database.cs
@@ -1,39 +1,45 @@
-﻿using Realms;
-using Windows.Storage;
-using Windows.System;
+﻿using LiteDB;
 
-namespace Model.DataPersistence; 
+namespace Model.DataPersistence;
 
-public class Status : RealmObject {
-    [PrimaryKey]
-    public string Id { get; set; } = string.Empty;
-    public string AccountId { get; set; } = string.Empty;
+public record struct Status {
+    [BsonId]
+    public string Id { get; set; }
+    public bool Favourited { get; set; }
+    public bool Reblogged { get; set; }
+    public bool Muted { get; set; }
+    public bool Bookmarked { get; set; }
+    public bool Pinned { get; set; }
+
+    public Status(Mastonet.Entities.Status apiStatus) {
+        Id = apiStatus.Id;
+        Favourited = apiStatus.Favourited ?? false;
+        Reblogged = apiStatus.Reblogged ?? false;
+        Muted = apiStatus.Muted ?? false;
+        Bookmarked = apiStatus.Bookmarked ?? false;
+        Pinned = apiStatus.Pinned ?? false;
+    }
 }
 
-public class User {
-    private static readonly StorageFolder dataFolder = ApplicationData.Current.LocalFolder;
+public class Timeline(LiteDatabase db, string account, string name) {
+    private readonly ILiteCollection<Status?> statuses = db.GetCollection<Status?>($"{account}:statuses");
+    private readonly ILiteCollection<string> statusKeys = db.GetCollection<string>($"{account}:{name}");
 
-    public static async Task<User?> CreateAsync(string userId) {
-        if (string.IsNullOrWhiteSpace(userId)) {
-            return null;
+    public IEnumerable<Status> StatusIterator {
+        get {
+            foreach (var id in statusKeys.FindAll()) {
+                var statusOrNull = statuses.FindById(id);
+                if (statusOrNull is Status status) {
+                    yield return status;
+                }
+            }
         }
-
-        var folder = await dataFolder.CreateFolderAsync("users", CreationCollisionOption.OpenIfExists);
-        var config = new RealmConfiguration(System.IO.Path.Combine(folder.Path, $"{userId}.realm"));
-        var userRealm = await Realm.GetInstanceAsync(config);
-
-        return new User(userId, userRealm);
-    } 
-
-    public string Id { get;  init; }
-    private Realm Realm { get; init; }
-
-    private User(string id, Realm realm) {
-        Id = id;
-        Realm = realm;
     }
+}
 
-    public async Task<Status?> GetStatus(string statusId) {
-        return Realm.Find<Status>(statusId);
-    }
+public class Account(LiteDatabase db, string id) {
+    public string Id => id;
+    public Timeline HomeTimeline { get; } = new(db, id, "home_timeline");
+    public Timeline FederatedTimeline { get; } = new(db, id, "federated_timeline");
+    public Timeline LocalTimeline { get; } = new(db, id, "local_timeline");
 }
