6d8e6c8ecc299cf60f880e5eefde2c146c5606da
Renn <8340896+AkazaRenn@users.noreply.github.com>
2026-09-19 18:06:40 -0400

    Remove TransparentButton in favor of SubtleButtonStyle

--- View/Controls/Components/TransparentButton.xaml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.UI.Xaml.Controls;
-
-namespace View.Controls.Components;
-
-internal sealed partial class TransparentButton: Button {
-    public TransparentButton() {
-        InitializeComponent();
-    }
-}
